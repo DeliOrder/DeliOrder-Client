c18f90dcc8e4212f2cbccf88f1aad9a77ece80cd
Yangsubo <sheeep555@gmail.com>
2024-08-12 17:24:32 +0900

    Build: Boilerplate 세팅

---
+++ src/main.cjs
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadURL("http://localhost:5173");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
