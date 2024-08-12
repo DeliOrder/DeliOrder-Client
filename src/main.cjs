250bced671ca299e565b13b4168b9f959abc29d0
Yangsubo <sheeep555@gmail.com>
2024-08-12 22:47:54 +0900

    Style(Component/App): Nav, Home 컴포넌트 뷰 구현

--- src/main.cjs
+++ src/main.cjs
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require("electron");
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
   });
 
   win.loadURL("http://localhost:5173");
