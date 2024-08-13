69408e96ae79c98e28c8ef0b26c6949654e0c427
coco8j <dlwndusco38@gmail.com>
2024-08-13 22:02:41 +0900

    Style: tailwind 속성 순서 정렬

--- src/Components/SignUp/index.jsx
+++ src/Components/SignUp/index.jsx
@@ -14,38 +14,38 @@ function SignUp() {
   };
 
   return (
-    <div className="flex-grow flex items-center justify-center p-4">
-      <form className="bg-white shadow-md rounded border-gray-600 p-6 m-4 w-[400px]">
+    <div className="flex flex-grow items-center justify-center p-4">
+      <form className="m-4 w-[400px] rounded border-gray-600 bg-white p-6 shadow-md">
         <div className="mb-4">
           <label
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="mb-2 block text-sm font-bold text-gray-700"
             htmlFor="user-mail"
           >
             이메일
           </label>
           <input
-            className="w-8/12 h-12 shadow border rounded-l p-3 text-gray-700 leading-tight focus:border-gray-700"
+            className="h-12 w-8/12 rounded-l border p-3 leading-tight text-gray-700 shadow focus:border-gray-700"
             id="user-mail"
             name="user-mail"
             type="email"
           />
           <button
-            className="w-4/12 h-12 bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-r focus:shadow-outline"
+            className="focus:shadow-outline h-12 w-4/12 rounded-r bg-blue-400 px-4 py-2 font-bold text-white hover:bg-blue-500"
             type="button"
-          //TODO: 추후 데이터베이스 연결 및 아이디 중복 확인 로직 구현
+            //TODO: 추후 데이터베이스 연결 및 아이디 중복 확인 로직 구현
           >
             중복확인
           </button>
         </div>
         <div className="mb-4">
           <label
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="mb-2 block text-sm font-bold text-gray-700"
             htmlFor="nickname"
           >
             닉네임
           </label>
           <input
-            className="w-full h-12 shadow border rounded p-3 text-gray-700 leading-tight focus:border-gray-700"
+            className="h-12 w-full rounded border p-3 leading-tight text-gray-700 shadow focus:border-gray-700"
             id="nickname"
             name="nickname"
             type="text"
@@ -53,13 +53,13 @@ function SignUp() {
         </div>
         <div className="mb-6">
           <label
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="mb-2 block text-sm font-bold text-gray-700"
             htmlFor="password"
           >
             비밀번호
           </label>
           <input
-            className="w-full h-12 shadow border rounded p-3 text-gray-700 leading-tight focus:border-gray-700"
+            className="h-12 w-full rounded border p-3 leading-tight text-gray-700 shadow focus:border-gray-700"
             id="password"
             name="password"
             type="password"
@@ -67,31 +67,35 @@ function SignUp() {
         </div>
         <div className="mb-6">
           <label
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="mb-2 block text-sm font-bold text-gray-700"
             htmlFor="password check"
           >
             비밀번호 확인
           </label>
           <input
-            className="w-full h-12 shadow border rounded p-3 text-gray-700 leading-tight focus:border-gray-700"
+            className="h-12 w-full rounded border p-3 leading-tight text-gray-700 shadow focus:border-gray-700"
             id="password-check"
             name="password-check"
             type="password-check"
           />
         </div>
         <a
-          className="inline-block m-2 font-bold text-sm text-blue-500 hover:underline"
+          className="m-2 inline-block text-sm font-bold text-blue-500 hover:underline"
           href="#"
         >
           개인정보 약관 보기
         </a>
-        <label className="mb-3 block text-gray-500 font-bold">
-          <input className="m-2 mb-6 leading-tight" name="checkbox" type="checkbox" />
+        <label className="mb-3 block font-bold text-gray-500">
+          <input
+            className="m-2 mb-6 leading-tight"
+            name="checkbox"
+            type="checkbox"
+          />
           <span className="text-sm">개인정보약관에 동의합니다.</span>
         </label>
-        <div className="flex items-center justify-center mb-2">
+        <div className="mb-2 flex items-center justify-center">
           <button
-            className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mb-4 rounded-md focus:shadow-outline"
+            className="focus:shadow-outline mb-4 w-full rounded-md bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
             type="button"
             //TODO: 현재 단순히 모달창을 여는 로직이며, 추후 서버와 연결하여 실제 가입 로직 구현
             onClick={openModal}
@@ -101,10 +105,10 @@ function SignUp() {
         </div>
       </form>
       <Modal isOpen={isModalOpen} onClose={navigateToMainPage}>
-        <h2 className="text-xl font-semibold mb-4">DELIORDER</h2>
+        <h2 className="mb-4 text-xl font-semibold">DELIORDER</h2>
         <p>신규회원가입을 축하합니다!</p>
         <button
-          className="mt-4 px-4 py-2 bg-blue-400 hover:bg-blue-500 text-white font-bold rounded-md focus:shadow-outline text-center"
+          className="focus:shadow-outline mt-4 rounded-md bg-blue-400 px-4 py-2 text-center font-bold text-white hover:bg-blue-500"
           onClick={navigateToMainPage}
         >
           메인페이지로 이동하기
