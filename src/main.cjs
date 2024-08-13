51ac83ce086b0484d1d82a0270cd33f375c575c3
Yangsubo <sheeep555@gmail.com>
2024-08-13 15:01:23 +0900

    Chore: dotenv 설치 및 적용

--- src/main.cjs
+++ src/main.cjs
@@ -1,12 +1,14 @@
 const { app, BrowserWindow } = require("electron");
+require("dotenv").config();
 
 const createWindow = () => {
+  const BASE_URL = process.env.VITE_BASE_URL;
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
   });
 
-  win.loadURL("http://localhost:5173");
+  win.loadURL(BASE_URL);
 };
 
 app.whenReady().then(() => {
