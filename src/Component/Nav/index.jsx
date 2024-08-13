3a80e510a7ba3fa356a23e6498400dcf4b613da6
Yangsubo <sheeep555@gmail.com>
2024-08-13 15:03:24 +0900

    Refactor: 불필요한 코드 제거 및 링크 태그 경로 "#"수정

--- src/Component/Nav/index.jsx
+++ src/Component/Nav/index.jsx
@@ -8,38 +8,32 @@ function Nav() {
           href="#"
           className="flex items-center font-semibold text-xl tracking-tight"
         >
-          <img src={DeliLogo} className="logo size-10" alt="Deli logo" />
+          <img src={DeliLogo} className="logo size-10" alt="DeliOrder logo" />
           DELIORDER
         </a>
       </div>
       <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
+        <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"></button>
       </div>
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div className="text-sm lg:flex-grow">
           <a
-            href="#responsive-header"
+            //TODO: 추후 소개 페이지 연결
+            href="#"
             className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
           >
             소개
           </a>
           <a
-            href="#responsive-header"
+            //TODO: 추후 보내기 페이지 연결
+            href="#"
             className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
           >
             보내기
           </a>
           <a
-            href="#responsive-header"
+            //TODO: 추후 받기 페이지 연결
+            href="#"
             className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
           >
             받기
@@ -47,12 +41,14 @@ function Nav() {
         </div>
         <div>
           <a
+            //TODO: 추후 내 소포함 페이지 연결
             href="#"
             className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0 mr-6"
           >
-            마이페이지
+            내 소포함
           </a>
           <a
+            //TODO: 추후 로그인 페이지 연결 및 로그아웃 로직 구현
             href="#"
             className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
           >
