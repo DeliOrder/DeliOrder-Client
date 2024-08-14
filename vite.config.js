396eb3b9b05abb6eec7708383313f1f56cb17f4d
Yangsubo <sheeep555@gmail.com>
2024-08-14 18:06:56 +0900

    Refactor: 절대경로 지정 및 파일 이름 변경

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: "@images",
+        replacement: path.resolve(__dirname, "src/assets/images"),
+      },
+    ],
+  },
 });
