149d4de11984d04c4390e76ff80689a81ffee42a
Yangsubo <sheeep555@gmail.com>
2024-08-14 18:06:16 +0900

    Refactor: CreatingPackage(구: PackagingOrders 컴포넌트)컴포넌트 내 모듈화

---
+++ src/Components/CreatingPackage/index.jsx
@@ -0,0 +1,13 @@
+import CreatingOrder from "./CreatingOrder";
+import PackagePreview from "./PackagePreview";
+
+function CreatingPackage() {
+  return (
+    <div className="relative flex min-h-max flex-row justify-start justify-items-stretch overflow-hidden bg-gray-50 px-3 py-6 sm:py-12">
+      <CreatingOrder />
+      <PackagePreview />
+    </div>
+  );
+}
+
+export default CreatingPackage;
