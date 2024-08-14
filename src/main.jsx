80a4344f7a329c90ff110b5d0dd9ee9b3365619d
coco8j <dlwndusco38@gmail.com>
2024-08-14 20:46:14 +0900

    Refactor: 파일 모듈 리팩토링

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./Components/App";
+import App from "./renderer/Components/App";
 import "./main.css";
 
 createRoot(document.getElementById("root")).render(
