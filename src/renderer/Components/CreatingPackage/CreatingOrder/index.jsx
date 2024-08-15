06dce32ea9f23d96a434535de70365ebad485b21
coco8j <dlwndusco38@gmail.com>
2024-08-15 15:41:20 +0900

    Refactor: form 의 각 input , tailwind css 모듈화

--- src/renderer/Components/CreatingPackage/CreatingOrder/index.jsx
+++ src/renderer/Components/CreatingPackage/CreatingOrder/index.jsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
 import BookmarkToolbar from "./BookmarkToolbar";
+import FilePicker from "../../shared/FilePicker";
+import FolderPicker from "../../shared/FolderPicker";
+import "../../shared/style.css";
 
 function CreatingOrder() {
+  const [selectedAction, setSelectedAction] = useState("");
+
+  const handleChange = (event) => {
+    setSelectedAction(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="relative w-2/5 bg-white px-6 pb-8 pt-10 shadow-sm ring-1 ring-gray-900/5 sm:ml-5 sm:max-w-lg sm:rounded-lg sm:px-10">
-      <label className="mb-2 block text-xl font-bold text-gray-700">
-        제조하기
-      </label>
-      <form>
-        <label className="mb-2 block text-base font-semibold text-gray-500">
-          행동 선택하기
-        </label>
+    <div className="container-small">
+      <label className="label-large">제조하기</label>
+      <form onSubmit={handleSubmit}>
+        <label className="label-small">행동 선택하기</label>
         <select
           name="action"
-          className="focus:shadow-outline block w-full appearance-none rounded border border-gray-300 bg-white px-4 py-3 pr-8 leading-tight text-gray-400 hover:border-gray-300 hover:bg-gray-100 focus:cursor-pointer focus:outline-none"
+          className="input-base focus:shadow-outline"
+          value={selectedAction}
+          onChange={handleChange}
         >
           <option value="">행동을 선택해주세요.</option>
           <option>생성하기</option>
@@ -22,26 +34,10 @@ function CreatingOrder() {
           <option>수정하기</option>
         </select>
         <div className="mx-auto max-w-md">
-          <label className="mb-2 block justify-items-start text-base font-semibold text-gray-500">
-            경로 선택하기
-          </label>
-          <button className="focus:shadow-outline block w-full rounded border border-gray-300 bg-white px-4 py-3 pr-8 leading-tight text-gray-400 hover:bg-gray-100 focus:cursor-pointer focus:outline-none">
-            경로를 선택해 주세요.
-          </button>
-          <label className="mb-2 block text-base font-semibold text-gray-500">
-            대상파일 선택하기
-          </label>
-          <input
-            type="file"
-            className="w-full cursor-pointer rounded border bg-white text-sm font-semibold text-gray-400 file:mr-4 file:cursor-pointer file:border-0 file:bg-gray-100 file:px-4 file:py-3 file:text-gray-500 file:hover:bg-gray-200"
-          />
-          <p className="mt-2 text-xs text-gray-400">
-            PNG, JPG SVG, WEBP, and GIF are Allowed.
-          </p>
+          <FilePicker />
+          <FolderPicker />
         </div>
-        <button className="my-3 w-full rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
-          조합하기
-        </button>
+        <button className="button-base-blue">조합하기</button>
         <BookmarkToolbar />
       </form>
     </div>
