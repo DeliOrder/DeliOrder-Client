5badd04b9938aff9737650ffb35446245a895930
coco8j <dlwndusco38@gmail.com>
2024-08-15 15:39:57 +0900

    Feat: 폴더 경로 선택 기능 구현

--- src/main/index.cjs
+++ src/main/index.cjs
@@ -1,11 +1,19 @@
-const { app, BrowserWindow } = require("electron");
 require("dotenv").config();
+
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const path = require("node:path");
 
 const createWindow = () => {
   const BASE_URL = process.env.VITE_BASE_URL;
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "../preload/index.cjs"),
+      contextIsolation: true,
+      enableRemoteModule: false,
+      nodeIntegration: false,
+    },
   });
 
   win.loadURL(BASE_URL);
@@ -13,4 +21,25 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow();
+
+  app.on("active", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+ipcMain.handle("open-file-dialog", async () => {
+  try {
+    const result = await dialog.showOpenDialog({
+      properties: ["openDirectory"],
+    });
+    console.log("main filePaths", result);
+    return result;
+  } catch (error) {
+    console.error("open-file-dialog handler:", error);
+    return { canceled: true, filePaths: [] };
+  }
 });
