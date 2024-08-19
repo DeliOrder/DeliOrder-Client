e2e70f10beaf1ff893d13bedc5795592b21c6efb
vlveqoo <vlveqoo@gmail.com>
2024-08-19 10:47:26 +0900

    Refactor: 함수를 분리하여 각 함수에서 단일 작업만 수행하도록 수정

--- src/renderer/Components/RecevingPackage/NumberInput/index.jsx
+++ src/renderer/Components/RecevingPackage/NumberInput/index.jsx
@@ -1,20 +1,20 @@
 import PropTypes from "prop-types";
 
-function NumberInput({ validateNumber, handleMoveNextInput }) {
+function NumberInput({ onKeyDownFunc, OnChangeFunc }) {
   return (
     <input
       maxLength="1"
       className="mx-3 h-16 w-16 transform rounded-lg bg-gray-200 text-center text-5xl shadow-md transition duration-200 hover:scale-110 focus:outline-none"
       type="text"
-      onKeyDown={(event) => validateNumber(event)}
-      onChange={(event) => handleMoveNextInput(event)}
+      onKeyDown={(event) => onKeyDownFunc(event)}
+      onChange={(event) => OnChangeFunc(event)}
     />
   );
 }
 
 NumberInput.propTypes = {
-  validateNumber: PropTypes.func.isRequired,
-  handleMoveNextInput: PropTypes.func.isRequired,
+  onKeyDownFunc: PropTypes.func.isRequired,
+  OnChangeFunc: PropTypes.func.isRequired,
 };
 
 export default NumberInput;
