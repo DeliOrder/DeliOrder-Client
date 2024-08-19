858919d8b2c0572c20a8c98a3cb5034582420c27
vlveqoo <vlveqoo@gmail.com>
2024-08-19 12:09:57 +0900

    Chore: 프로젝트 내부규칙에 맞도록 표기법 등 수정

--- src/renderer/Components/RecevingPackage/NumberInput/index.jsx
+++ src/renderer/Components/RecevingPackage/NumberInput/index.jsx
@@ -1,20 +1,20 @@
 import PropTypes from "prop-types";
 
-function NumberInput({ onKeyDownFunc, OnChangeFunc }) {
+function NumberInput({ onKeyDownFunc, onChangeFunc }) {
   return (
     <input
       maxLength="1"
       className="mx-3 h-16 w-16 transform rounded-lg bg-gray-200 text-center text-5xl shadow-md transition duration-200 hover:scale-110 focus:outline-none"
       type="text"
       onKeyDown={(event) => onKeyDownFunc(event)}
-      onChange={(event) => OnChangeFunc(event)}
+      onChange={(event) => onChangeFunc(event)}
     />
   );
 }
 
 NumberInput.propTypes = {
   onKeyDownFunc: PropTypes.func.isRequired,
-  OnChangeFunc: PropTypes.func.isRequired,
+  onChangeFunc: PropTypes.func.isRequired,
 };
 
 export default NumberInput;
