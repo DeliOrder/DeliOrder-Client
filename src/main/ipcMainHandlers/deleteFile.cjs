a6d8ac88ff1b6cc93f359006c35e0ff6dec7946d
coco8j <dlwndusco38@gmail.com>
2024-08-20 20:17:18 +0900

    Chore: 함수명 변경

---
+++ src/main/ipcMainHandlers/deleteFile.cjs
@@ -0,0 +1,26 @@
+const { ipcMain } = require("electron");
+const path = require("path");
+const fs = require("fs");
+
+const { convertPath } = require("../utils/convertPath.cjs");
+
+const deleteFile = () => {
+  ipcMain.handle("delete-file", async (event, order) => {
+    try {
+      const fullPath = path.join(order.executionPath, order.attachmentName);
+      const filePath = convertPath(fullPath);
+
+      if (!fs.existsSync(filePath)) {
+        throw new Error("해당 위치에 요청한 파일이 없습니다.");
+      }
+
+      const trash = (await import("trash")).default;
+      await trash(filePath);
+
+      return;
+    } catch (error) {
+      console.error("delete-file main handler error:", error);
+    }
+  });
+};
+module.exports = { deleteFile };
