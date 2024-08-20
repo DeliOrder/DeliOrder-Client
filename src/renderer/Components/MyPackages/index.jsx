42c4127b45c079751b164180cb88789aefa30dea
vlveqoo <vlveqoo@gmail.com>
2024-08-20 13:36:38 +0900

    Feat: 최신 및 과거 순 정렬 버튼 추가

--- src/renderer/Components/MyPackages/index.jsx
+++ src/renderer/Components/MyPackages/index.jsx
@@ -1,14 +1,43 @@
+import { useState } from "react";
+
 import mockData from "./mockData.json";
+import triangleArrowDown from "@images/triangleArrowDown.svg";
 
 function MyPackages() {
   // TODO: userDBAllData는 목데이터로 front 로직 구현시 데이터베이스에서 실제정보를 불러와주는 로직 필요
   const userDBAllData = mockData;
-  const latestOrderUserHistory = userDBAllData.history.slice().reverse();
+  const [currentSort, setCurrentSort] = useState("sortByNewest");
+  const [userHistorySort, setUserHistorySort] = useState(
+    userDBAllData.history.slice().reverse(),
+  );
+
+  const toggleSort = () => {
+    if (currentSort === "sortByNewest") {
+      setCurrentSort("sortByOldest");
+      setUserHistorySort(userDBAllData.history);
+    } else {
+      setCurrentSort("sortByNewest");
+      setUserHistorySort(userDBAllData.history.slice().reverse());
+    }
+  };
+
   return (
     <div className="flex min-h-screen items-start justify-center bg-gray-50 p-8">
       <div className="mt-6 w-full max-w-3xl space-y-6">
-        {/* <div className="text-lg font-bold text-gray-700">▽ 과거행동순</div> */}
-        {latestOrderUserHistory.map((userPackage, index) => (
+        <div
+          className="flex cursor-pointer flex-row items-center text-lg font-bold text-gray-700"
+          onClick={toggleSort}
+        >
+          <img
+            className="mr-1 h-4 w-4"
+            src={triangleArrowDown}
+            alt="triangleArrowDown"
+          />
+          {currentSort === "sortByNewest"
+            ? "최신패키지 순 정렬"
+            : "과거패키지 순 정렬"}
+        </div>
+        {userHistorySort.map((userPackage, index) => (
           <div
             key={userPackage.serialNumber}
             className="rounded-lg border border-gray-300 bg-white p-6 shadow-md transition-shadow duration-300 hover:shadow-lg"
@@ -36,7 +65,7 @@ function MyPackages() {
             </div>
             <div className="space-y-2">
               {userPackage.orders.map((order, orderIndex) => (
-                <div key={order.createdAt} className="text-gray-700">
+                <div key={order.createdAt.$date} className="text-gray-700">
                   {orderIndex + 1}. {order.action}
                 </div>
               ))}
