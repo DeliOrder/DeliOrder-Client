d654d171ef4e12370563846ef1e69e4a821d744c
coco8j <dlwndusco38@gmail.com>
2024-08-22 14:03:50 +0900

    Fix: 모달 정상 출력을 위한 z 값 부여

--- src/renderer/Components/Modal/index.jsx
+++ src/renderer/Components/Modal/index.jsx
@@ -4,8 +4,8 @@ function Modal({ isOpen, children }) {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="relative flex min-w-[300px] items-center justify-center rounded-lg bg-white p-6 shadow-lg">
+    <div className="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="relative z-20 flex min-w-[300px] items-center justify-center rounded-lg bg-white p-6 shadow-lg">
         <div className="flex-col items-center justify-center">{children}</div>
       </div>
     </div>
