ce81ca35e0abc668836a5016d07109868edd62c5
coco8j <dlwndusco38@gmail.com>
2024-08-22 14:04:37 +0900

    Feat: 북마크(즐겨찾기) client side 로직 구현 완료

--- src/renderer/Components/CreatingPackage/CreatingOrder/BookmarkToolbar/index.jsx
+++ src/renderer/Components/CreatingPackage/CreatingOrder/BookmarkToolbar/index.jsx
@@ -1,19 +1,41 @@
 import axios from "axios";
+import { useState } from "react";
 
 import usePackageStore from "@renderer/store";
+import Modal from "../../../Modal";
 
 import addingIcon from "@images/addingIcon.svg";
 import downloadIcon from "@images/downloadIcon.svg";
 
 function BookmarkToolbar() {
-  const getOrder = usePackageStore((state) => state.getOrder);
-  const BookmarkTarget = getOrder();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [bookmarks, setBookMarks] = useState([]);
+  const {
+    clientStatus: { isLogin },
+    getOrder,
+    updateOrder,
+  } = usePackageStore();
 
-  const handleAddBookmark = () => {
-    // TODO: 유저아이디 받아오는 로직 추가 구현 필요
-    axios.post(
-      `${import.meta.env.VITE_SERVER_URL}/${"user-id"}/bookmark`,
-      BookmarkTarget,
+  const userId = localStorage.getItem("userID");
+  const notifyLoginRequired = () => {
+    alert("로그인 유저 전용 기능입니다.");
+  };
+
+  const handleAddBookmark = async () => {
+    if (!isLogin) {
+      notifyLoginRequired();
+      return;
+    }
+    const BookmarkTarget = getOrder();
+    const formattedBookmarkData = {
+      ...BookmarkTarget,
+      attachmentType: "",
+      attachmentUrl: "",
+    };
+
+    await axios.post(
+      `${import.meta.env.VITE_SERVER_URL}/${userId}/bookmark`,
+      formattedBookmarkData,
       {
         headers: {
           "Content-Type": "application/json",
@@ -21,29 +43,75 @@ function BookmarkToolbar() {
       },
     );
   };
+
+  const handleGetBookmark = async () => {
+    if (!isLogin) {
+      notifyLoginRequired();
+      return;
+    }
+    const BookmarkTarget = getOrder();
+    const result = await axios.post(
+      `${import.meta.env.VITE_SERVER_URL}/${userId}/bookmark`,
+      BookmarkTarget,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+
+    setBookMarks(result);
+    setIsModalOpen(true);
+  };
+
+  const applyBookmark = (index) => {
+    updateOrder(bookmarks[index]);
+    setIsModalOpen(false);
+  };
+
   return (
     <>
+      {bookmarks.length > 0 && (
+        <Modal isOpen={isModalOpen}>
+          {bookmarks.map((bookmark, index) => (
+            <button
+              key={bookmark.attachmentName}
+              className="button-base-blue"
+              onClick={() => {
+                applyBookmark(index);
+              }}
+            >
+              {bookmark.action}
+            </button>
+          ))}
+          <button
+            className="button-yellow-square"
+            onClick={() => {
+              setIsModalOpen(false);
+            }}
+          >
+            닫기
+          </button>
+        </Modal>
+      )}
       <span className="label-large">즐겨찾기</span>
       <div className="flex flex-row justify-around py-1">
         <div className="flex flex-row">
-          <span className="block content-center px-3 text-sm text-gray-500">
-            추가하기
-          </span>
+          <span className="text-sm-gray-centered">추가하기</span>
           <button
             type="button"
-            className="inline-flex h-10 w-10 items-center justify-center rounded-full border-none bg-blue-600 outline-none hover:bg-blue-700 active:bg-blue-600"
+            className="button-blue-round"
             onClick={handleAddBookmark}
           >
             <img src={addingIcon} alt="adding Icon" />
           </button>
         </div>
         <div className="flex flex-row">
-          <span className="block content-center px-3 text-sm text-gray-500">
-            가져오기
-          </span>
+          <span className="text-sm-gray-centered">가져오기</span>
           <button
             type="button"
-            className="inline-flex h-10 w-10 items-center justify-center rounded border-none bg-yellow-400 outline-none hover:bg-yellow-600 active:bg-yellow-500"
+            className="button-yellow-square"
+            onClick={handleGetBookmark}
           >
             <img src={downloadIcon} alt="download Icon" />
           </button>
