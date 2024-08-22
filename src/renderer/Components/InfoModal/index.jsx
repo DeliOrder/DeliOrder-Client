60bceb232407016e858efa8dc5b0cd2bbdc39fe0
vlveqoo <vlveqoo@gmail.com>
2024-08-22 20:37:44 +0900

    Feat: 인포메세지용 모달 컴포넌트 추가

---
+++ src/renderer/Components/InfoModal/index.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+
+function InfoModal({ isOpen, children }) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="relative z-20 flex min-w-[300px] items-center justify-center rounded-lg bg-white p-6 shadow-lg">
+        <div className="flex-col items-center justify-center">{children}</div>
+      </div>
+    </div>
+  );
+}
+
+InfoModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  children: PropTypes.node,
+};
+
+export default InfoModal;
