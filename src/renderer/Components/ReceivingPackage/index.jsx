be824613988812d1bff4d8caa29222bbed06c240
Yangsubo <sheeep555@gmail.com>
2024-08-23 10:33:37 +0900

    Refactor: 패키지 불러오기 input 칸 움직여주기 로직 보완

--- src/renderer/Components/ReceivingPackage/index.jsx
+++ src/renderer/Components/ReceivingPackage/index.jsx
@@ -36,7 +36,15 @@ function ReceivingPackage() {
     }
   };
   const updateInputNumbers = (event, index) => {
-    if (!isNaN(event.key) && event.key.trim() !== "") {
+    if (event.target.value && event.code !== "Backspace") {
+      return event.target.nextSibling?.focus();
+    }
+
+    if (!event.target.value && event.code === "Backspace") {
+      return event.target.previousSibling?.focus();
+    }
+
+    if (!Number.isNaN(event.key) && event.key.trim() !== "") {
       const tempNumbers = [...inputNumbers];
       tempNumbers[index] = event.key;
       setInputNumbers(tempNumbers);
