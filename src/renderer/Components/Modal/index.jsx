d71ddefb88724e43dea438381bc97275b4698651
coco8j <dlwndusco38@gmail.com>
2024-08-24 17:13:24 +0900

    Feat: 패키지 실행 결과를 보여줍니다.

--- src/renderer/Components/Modal/index.jsx
+++ src/renderer/Components/Modal/index.jsx
@@ -1,20 +1,42 @@
 import PropTypes from "prop-types";
 
-function Modal({ isOpen, children }) {
+function Modal({ modalConfig }) {
+  let { isOpen, title, content, onClose } = modalConfig;
+
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50">
       <div className="relative z-20 flex min-w-[300px] items-center justify-center rounded-lg bg-white p-6 shadow-lg">
-        <div className="flex-col items-center justify-center">{children}</div>
+        <div className="flex-col items-center justify-center">
+          <h2 className="mb-4 text-xl font-semibold">{title}</h2>
+          {content}
+          <button
+            type="button"
+            onClick={() => onClose()}
+            className="button-yellow-square w-24"
+          >
+            닫기
+          </button>
+        </div>
       </div>
     </div>
   );
 }
 
 Modal.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
-  children: PropTypes.node,
+  modalConfig: PropTypes.shape({
+    isOpen: PropTypes.bool.isRequired,
+    title: PropTypes.string,
+    content: PropTypes.node,
+    onClose: PropTypes.func,
+  }).isRequired,
+};
+
+Modal.defaultProps = {
+  modalConfig: {
+    onClose: () => {},
+  },
 };
 
 export default Modal;
