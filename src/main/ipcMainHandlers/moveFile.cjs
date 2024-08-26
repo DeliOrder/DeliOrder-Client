06300ba05c0b73d2af4dce2283886ed3ec60180e
coco8j <dlwndusco38@gmail.com>
2024-08-26 11:32:23 +0900

    Fix: git merge 충돌 해결

--- src/main/ipcMainHandlers/moveFile.cjs
+++ src/main/ipcMainHandlers/moveFile.cjs
@@ -7,18 +7,14 @@ const { convertPath } = require("../utils/convertPath.cjs");
 const moveFile = () => {
   ipcMain.handle("move-file", async (event, order) => {
     try {
-<<<<<<< HEAD
-      const oldFullPath =
-        order.attachmentType === "folder"
-          ? path.join(order.sourcePath)
-          : path.join(order.sourcePath, order.attachmentName);
-=======
       if (order.action !== "이동하기") {
         return "수신받은 행동이 '이동하기'가 아닙니다.";
       }
 
-      const oldFullPath = path.join(order.sourcePath, order.attachmentName);
->>>>>>> 42310f0 (Feat: 각 오더의 결과값을 받아올 수 있도록 로직 구현)
+      const oldFullPath =
+        order.attachmentType === "folder"
+          ? path.join(order.sourcePath)
+          : path.join(order.sourcePath, order.attachmentName);
       const newFullPath = path.join(order.executionPath, order.attachmentName);
 
       const convertedFolderPath = convertPath(order.executionPath);
@@ -29,18 +25,9 @@ const moveFile = () => {
         fs.mkdirSync(convertedFolderPath, { recursive: true });
       }
 
-<<<<<<< HEAD
       fs.renameSync(convertedOldFullPath, convertedNewFullPath);
-=======
-      fs.rename(convertedOldFullPath, convertedNewFullPath, (error) => {
-        if (error) {
-          console.error("파일 이동 중 오류 발생:", error);
-          return;
-        }
-        console.error(`파일이 ${convertedNewFullPath}로 이동하였습니다.`);
-        return "이동 성공";
-      });
->>>>>>> 42310f0 (Feat: 각 오더의 결과값을 받아올 수 있도록 로직 구현)
+
+      return "이동 성공";
     } catch (error) {
       console.error("moving-file main handler 에러:", error);
       return "이동 실패";
--- src/renderer/Components/ReceivingPackage/index.jsx
+++ src/renderer/Components/ReceivingPackage/index.jsx
@@ -1,67 +1,12 @@
-import { useState } from "react";
 import axios from "axios";
+import { useState } from "react";
 
 import Modal from "../Modal";
 import NumberInput from "./NumberInput";
 import PreNotification from "./PreNotification";
 
-<<<<<<< HEAD
-function ReceivingPackage() {
-  const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
-  const [infoModalMessage, setInfoModalMessage] = useState("");
-  const [processResults, setProcessResults] = useState([]);
-  const [isNotificationOpen, setIsNotificationOpen] = useState(false);
-<<<<<<< HEAD
-  const [modalMessage, setModalMessage] = useState("");
-  const [orders, setOrders] = useState([]);
-
-  const navigate = useNavigate();
-
-  const validateNumber = (event) => {
-    const VALID_KEY = [
-      "Tab",
-      "Backspace",
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "0",
-    ];
-
-    if (!VALID_KEY.includes(event.key) || event.key === " ") {
-      event.preventDefault();
-    }
-  };
-  const updateInputNumbers = (event) => {
-    if (event.target.value && event.code !== "Backspace") {
-      return event.target.nextSibling?.focus();
-    }
-
-    if (!event.target.value && event.code === "Backspace") {
-      return event.target.previousSibling?.focus();
-    }
-  };
-
-  const handleFocusShift = (event) => {
-    if (event.nativeEvent.data === null) {
-      event.target.previousSibling?.focus();
-      return;
-    }
-
-    if (event) {
-      event.target.nextSibling?.focus();
-    }
-  };
-
-=======
-=======
-import useModalConfig from "../../utils/useModalConfig";
->>>>>>> a64b85c (Feat: 패키지 실행 결과를 보여줍니다.)
+import useModal from "@renderer/utils/useModal";
+import { SERIAL_NUMBER_LENGTH } from "@renderer/constants/config";
 
 function ReceivingPackage() {
   const [infoMessage, setInfoMessage] = useState("");
@@ -75,114 +20,23 @@ function ReceivingPackage() {
   const [inputNumbers, setInputNumbers] = useState(
     Array(SERIAL_NUMBER_LENGTH).fill(""),
   );
-
-  const { modalConfig: resultModalConfig, openModal: openResultModal } =
-    useModalConfig();
-
-  const { modalConfig: infoModalConfig, openModal: openInfoModal } =
-    useModalConfig();
-
-  const {
-    modalConfig: confirmModalConfig,
-    openModal: openConfirmModal,
-    closeModal: closeConfirmModal,
-  } = useModalConfig();
-
-  const navigate = useNavigate();
->>>>>>> 42310f0 (Feat: 각 오더의 결과값을 받아올 수 있도록 로직 구현)
-  const navigateToMainPage = () => {
-    navigate("/");
-  };
-
-  const handleGetOrder = async (event) => {
+  const handleGetPackage = async (event) => {
     event.preventDefault();
 
     try {
-      const inputNumbers = Array.from(
-        event.target.elements,
-        (element) => element.value,
-      );
       const serialNumber = inputNumbers.join("");
       const {
         data: {
-          existPackage: { orders: orderList },
-          message,
+          existPackage: { orders },
         },
       } = await axios.get(
         `${import.meta.env.VITE_SERVER_URL}/packages/${serialNumber}`,
       );
-      console.log("조회완료");
-      const currentPackage = response.data.existPackage.orders;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-      if (orderList) {
-        setOrders(orderList);
-        setIsNotificationOpen(true);
+      if (orders) {
+        setCurrentPackage(orders);
+        openConfirmModal();
       }
-
-      if (!handleConfirmReply) {
-        setModalMessage("취소합니다.");
-        setIsAlertModalOpen(true);
-      }
-
-      const processActions = async () => {
-        for (const order of orderList) {
-          switch (order.action) {
-            case "생성하기":
-              await window.electronAPI.downloadFile(order);
-              break;
-            case "이동하기":
-              await window.electronAPI.moveFile(order);
-              break;
-            case "복제하기":
-              await window.electronAPI.replicateFile(order);
-              break;
-            case "수정하기":
-              await window.electronAPI.editFileName(order);
-              break;
-            case "실행하기":
-              await window.electronAPI.executeFile(order);
-              break;
-            case "삭제하기":
-              await window.electronAPI.deleteFile(order);
-              break;
-            default:
-              console.error("입력 되지 않은 행동입니다.");
-          }
-        }
-      };
-
-      processActions();
-      setModalMessage(response.data.message);
-      setIsAlertModalOpen(true);
-=======
-      const orderList = response.data.existPackage.orders;
-      setCurrentOrders(orderList);
-
-      setIsNotificationOpen(true);
->>>>>>> 42310f0 (Feat: 각 오더의 결과값을 받아올 수 있도록 로직 구현)
-=======
-      openConfirmModal(
-        <>
-          <PreNotification orders={currentPackage} />
-          <p className="my-2 flex justify-around">
-            <button
-              type="button"
-              onClick={() => handleProcessPackage(currentPackage)}
-              className="button-yellow-square w-24"
-            >
-              확인
-            </button>
-          </p>
-        </>,
-        "패키지 내용 확인",
-        navigateToMainPage,
-      );
->>>>>>> a64b85c (Feat: 패키지 실행 결과를 보여줍니다.)
-
-      setCurrentPackage(response.data.existPackage.orders);
-      openConfirmModal();
     } catch (error) {
       setInfoMessage(
         error.response
@@ -233,7 +87,7 @@ function ReceivingPackage() {
   return (
     <div className="flex h-[90.5vh] items-center justify-center bg-blue-100">
       <form
-        onSubmit={handleReceivePackage}
+        onSubmit={handleGetPackage}
         className="flex h-3/5 w-3/5 flex-col items-center gap-20 rounded-xl bg-white p-10 shadow-2xl"
       >
         <label className="text-6xl font-semibold tracking-wide text-gray-800">
@@ -245,56 +99,44 @@ function ReceivingPackage() {
             .map((_, index) => (
               <NumberInput
                 key={index}
-<<<<<<< HEAD
-                onKeyDownFunc={(event) => {
-                  validateNumber(event);
-                  updateInputNumbers(event);
-                }}
-                onChangeFunc={handleFocusShift}
-=======
                 setInputNumbers={setInputNumbers}
                 inputNumbers={inputNumbers}
                 index={index}
->>>>>>> 42310f0 (Feat: 각 오더의 결과값을 받아올 수 있도록 로직 구현)
               />
             ))}
         </div>
-        <button
-          type="submit"
-          onClick={handleGetPackage}
-          className="button-slate-round"
-        >
+        <button type="submit" className="button-slate-round">
           받기
         </button>
-        <Modal title={"오류"} isOpen={isInfoModalOpen} onClose={closeInfoModal}>
-          <p>{infoMessage}</p>
-        </Modal>
-        <Modal
-          title={"패키지 내용 확인"}
-          isOpen={isConfirmModalOpen}
-          onClose={closeConfirmModal}
-        >
-          <PreNotification orders={currentPackage} />
-          <button
-            type="button"
-            onClick={() => handleProcessPackage(currentPackage)}
-            className="button-yellow-square w-24"
-          >
-            확인
-          </button>
-        </Modal>
-        <Modal
-          title="실행결과"
-          isOpen={isResultModalOpen}
-          onClose={closeResultModal}
-        >
-          {processResults.map((result, index) => (
-            <p key={index} className="mt-2 text-xs">
-              {result}
-            </p>
-          ))}
-        </Modal>
       </form>
+      <Modal title={"오류"} isOpen={isInfoModalOpen} onClose={closeInfoModal}>
+        <p>{infoMessage}</p>
+      </Modal>
+      <Modal
+        title={"패키지 내용 확인"}
+        isOpen={isConfirmModalOpen}
+        onClose={closeConfirmModal}
+      >
+        <PreNotification orders={currentPackage} />
+        <button
+          type="button"
+          onClick={() => handleProcessPackage(currentPackage)}
+          className="button-yellow-square w-24"
+        >
+          확인
+        </button>
+      </Modal>
+      <Modal
+        title="실행결과"
+        isOpen={isResultModalOpen}
+        onClose={closeResultModal}
+      >
+        {processResults.map((result, index) => (
+          <p key={index} className="mt-2 text-xs">
+            {result}
+          </p>
+        ))}
+      </Modal>
     </div>
   );
 }
