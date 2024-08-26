bf412824f44610c7f8fdec5a933fcb07330fa1c9
coco8j <dlwndusco38@gmail.com>
2024-08-26 11:57:54 +0900

    Style: 버튼 중앙정렬

--- src/renderer/Components/Modal/index.jsx
+++ src/renderer/Components/Modal/index.jsx
@@ -6,16 +6,18 @@ function Modal({ title = "", isOpen, onClose, children }) {
   return (
     <div className="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50">
       <div className="relative z-20 flex min-w-[300px] items-center justify-center rounded-lg bg-white p-6 shadow-lg">
-        <div className="flex-col items-center justify-center">
+        <div className="flex-col content-center items-center">
           <h2 className="mb-4 text-xl font-semibold">{title}</h2>
           {children}
-          <button
-            type="button"
-            onClick={onClose}
-            className="button-yellow-square mt-5 w-24"
-          >
-            닫기
-          </button>
+          <div className="flex justify-center">
+            <button
+              type="button"
+              onClick={onClose}
+              className="button-yellow-square mt-5 w-24"
+            >
+              닫기
+            </button>
+          </div>
         </div>
       </div>
     </div>
