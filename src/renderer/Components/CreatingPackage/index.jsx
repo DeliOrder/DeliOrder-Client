4935c2b324b150c6dc4b9c7151fbbc9a17014595
Yangsubo <sheeep555@gmail.com>
2024-10-14 21:58:29 +0900

    Feat(CreatingPackage): 언마운트시 상태값 초기화 구현

--- src/renderer/Components/CreatingPackage/index.jsx
+++ src/renderer/Components/CreatingPackage/index.jsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
 import CreatingOrder from "./CreatingOrder";
 import PackagePreview from "./PackagePreview";
+import usePackageStore from "../../store";
 
 function CreatingPackage() {
+  const { clearOrder } = usePackageStore();
+
+  useEffect(() => {
+    return () => clearOrder();
+  }, [clearOrder]);
+
   return (
-    <div className="bg-gray-light relative flex min-h-fit flex-1 flex-row items-stretch px-3 py-6">
+    <div className="relative flex min-h-fit flex-1 flex-row items-stretch bg-gray-light px-3 py-6">
       <CreatingOrder />
       <PackagePreview />
     </div>
